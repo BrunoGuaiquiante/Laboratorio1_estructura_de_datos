d0fcdadc6becae7f0f8c630d19d18951cc203560
Your Name <you@example.com>
2026-08-16 23:15:44 +0000

    new try

--- exercises.c
+++ exercises.c
@@ -43,6 +43,13 @@ Al finalizar retorna la lista creada.
 
 List* crea_lista() {
    List* L = create_list();
+   
+   for(int i=1;i<=10;i++){
+      void* n=(void*)malloc(size_type(int));
+      n=i;
+      pushBack(L,n);
+   }
+   
    return L;
 }
 
